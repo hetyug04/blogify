5d0502d1eb1ad4328f84a87e3457654d52ed76c2
hetyug04 <87499612+hetyug04@users.noreply.github.com>
2023-06-14 16:08:34 -0400

    Update postRouter.js

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -2,7 +2,15 @@ import express from "express";
 const router = express.Router()
 import { post, updatePost, getAllPosts, getPost, deletePost} from "../controllers/postController.js";
 
-router.route('/post').post(post)
+import { rateLimit } from 'express-rate-limit';
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    message: "Too many requests from this IP address, please try again later"
+})
+
+router.route('/post').post(limiter,post)
 router.route('/updatePost').patch(updatePost)
 router.route('/getAllPosts').get(getAllPosts)
 router.route('/getPost').post(getPost)
