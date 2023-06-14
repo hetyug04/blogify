0c2ade507ffc427d644fac8db8cc0a00c87dd1ae
hetyug04 <87499612+hetyug04@users.noreply.github.com>
2023-06-14 16:07:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config("");
 import connectDB from "./db/connect.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+import helmet from 'helmet'
+import xss from 'xss-clean'
+import mongoSanitize from "express-mongo-sanitize";
 
 import notFoundMiddleWare from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
@@ -11,8 +17,13 @@ import authRouter from "./routers/authRouter.js";
 import userRouter from "./routers/userRouter.js";
 import postRouter from "./routers/postRouter.js";
 
-app.use(express.json());
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.json());
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
 //middleware
 app.get("/", (req, res) => {
   res.send("Welcome");
@@ -27,6 +38,11 @@ app.get("/api/v1", (req, res) => {
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/post", postRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 app.use(notFoundMiddleWare);
 app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5000;
@@ -35,7 +51,7 @@ const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
     app.listen(port, () => {
-     console.log(`Server is listening at Port: ${port}`);
+      console.log(`Server is listening at Port: ${port}`);
     });
   } catch (error) {
     error;
